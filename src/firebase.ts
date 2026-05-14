6fb487742d75b6e1457b51fcf330fa1554910e26
souta <s.hirao@greendice.co.jp>
2026-05-14 17:27:52 +0900

    a

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,14 +7,8 @@ export const realtimeDatabaseUrl =
   import.meta.env.VITE_FIREBASE_DATABASE_URL ||
   'https://continue-game-default-rtdb.asia-southeast1.firebasedatabase.app'
 
-const firebaseApiKey = import.meta.env.VITE_FIREBASE_API_KEY
-
-if (!firebaseApiKey) {
-  throw new Error('Missing VITE_FIREBASE_API_KEY')
-}
-
 const firebaseConfig = {
-  apiKey: firebaseApiKey,
+  apiKey: 'AIzaSyDwCogYTgY_k4JYM6dTEb2oB1NjqRF0OPg',
   authDomain: 'continue-game.firebaseapp.com',
   databaseURL: realtimeDatabaseUrl,
   projectId: 'continue-game',
