17906978d17e4b52516369c8a61acf0429310ccd
souta <s.hirao@greendice.co.jp>
2026-06-05 18:22:20 +0900

    JPEGの

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'continue-tablet-v32'
-const STATIC_IMAGE_VERSION = 'images-20260605-3'
+const CACHE_NAME = 'continue-tablet-v33'
+const STATIC_IMAGE_VERSION = 'images-20260605-4'
 const BACKUP_PHOTO_VERSION = 'backup-photos-20260604-1'
 
 function versionedAsset(path) {
--- src/App.tsx
+++ src/App.tsx
@@ -83,9 +83,9 @@ function versionedAsset(path: string, version: string) {
   return publicAsset(`${path}${separator}v=${version}`)
 }
 
-const SCENE_ONE_VIDEO_VERSION = 'scene-1-20260605-2'
-const APP_CACHE_NAME = 'continue-tablet-v32'
-const STATIC_IMAGE_VERSION = 'images-20260605-3'
+const SCENE_ONE_VIDEO_VERSION = 'scene-1-20260605-3'
+const APP_CACHE_NAME = 'continue-tablet-v33'
+const STATIC_IMAGE_VERSION = 'images-20260605-4'
 const BACKUP_PHOTO_VERSION = 'backup-photos-20260604-1'
 const BACKUP_PHOTO_FOLDER = 'images/backup-photos'
 const BACKUP_PHOTO_MANIFEST_PATH = `${BACKUP_PHOTO_FOLDER}/backup-photos.json`
